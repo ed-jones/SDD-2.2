9ed0753cf39b16a65181ee88ec5fe10d1ce8604a
Ed Jones <edjones1999@gmail.com>
2017-06-20 12:12:31 +1000

    fixed style in selection.py

--- selection.py
+++ selection.py
@@ -1,13 +1,14 @@
+"Selection Sort"
+
 def selection(array):
-    n = len(array)
-    for j in range(n-1):
-        iMin = j
-        for i in range(j+1,n):
-            if (array[i] < array[iMin]):
-                iMin = i
-        array[j], array[iMin] = array[iMin], array[j]
-        
+    "sorts an array based on size"
+    for j in range(len(array)-1):
+        imin = j
+        for i in range(j+1, len(array)):
+            if array[i] < array[imin]:
+                imin = i
+        array[j], array[imin] = array[imin], array[j]
 
     return array
 
-print(selection([22,43,111,1,23,67]))
+print(selection([22, 43, 111, 1, 23, 67]))
